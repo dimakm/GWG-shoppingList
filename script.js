a2bc12e5c13372296e52283a8eed03b57bf342ab
Dima Kroma <37256188+dimakm@users.noreply.github.com>
2018-03-24 12:37:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 
 // Create a new list item whith the amount 
 function newElement() {
-  let  li = document.createElement("li");
+  //create new list item
+  let  li = document.createElement("li"); 
+  // assign the value in the input to shoppingItem
   const shoppingItem = document.getElementById("myItem").value;
+  // assign the value in the quantity input to quantity
   const quantity = document.getElementById("theAmount").value;
+   // create a string with the item and quantity
   const inputVal = shoppingItem + ' ' + quantity  ;
   
   let n = document.createTextNode(inputVal);
-  li.appendChild(n);
-  if (inputVal === '') {
+  li.appendChild(n); 
+  if (shoppingItem === '') { // test if the shoopping item input field is empty then show an alert
     alert("You must enter something!");
   } else {
   document.getElementById("theListUL").appendChild(li);
