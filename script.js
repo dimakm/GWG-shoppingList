fc46de7b56b0142c9b5c4238a9b8faeecb8706d0
Kim McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-04-04 11:19:15 -0400

    Current files

--- script.js
+++ script.js
@@ -2,40 +2,42 @@ $(document).ready(function() {
   console.log("DOMContent fully loaded.");
 
   function addListItem() {
+		$('.itemAlert').remove();
     var shoppingItem = $("#myItem").val();
     var quantity = $("#quantity").val();
+    var unit= $("#unit").val();
     //Regular expression below checks for digits, used for error checking the shoppingItem
-    // \d+ checks for one or more digits and 'g' means global, checking for all occurrences of a digit
+    // \d+ checks for one or more digits and 'g' means global, checking for all occurreances of a digit
     var digits = /\d+/g;
 
     if (shoppingItem === '') {
-      alert("You must enter something!");
+			$('#unit').after('<p class="itemAlert">You must enter something !</p>');
     } /* No item entered */
 
     else if(shoppingItem.match(digits)){
-      alert("Not an item, please try again.");
-    }/* If there is any digit in the shoppingItem string, alert is called*/
+			$('#unit').after('<p class="itemAlert">Not an item, please try again .</p>');
+    }/* If there is any digit in the shoppingItem string, warning is displayed*/
 
     else {
       if((quantity > 1 && shoppingItem.substr(shoppingItem.length - 1) == 's')) {
-        shoppingItem = shoppingItem.substr(0, shoppingItem.length - 1);
-        $("#myUL").append("<li> <input type='checkbox' name='done' class='itemDone'/> (x" + quantity + ") " + shoppingItem.toLowerCase() + " </li>" );
+      /*  shoppingItem = shoppingItem.substr(0, shoppingItem.length - 1);*/
+        $("#myUL").append("<li> <input type='checkbox' name='done' class='itemDone'/> ( " + quantity + unit + "s ) " + shoppingItem + " <button class='delete'> x </button> </li>" );
       } /* More than 1 quantity, and the last character IS an 's', drop the 's'. Reassign the shoppingItem string, starting from the first character (at 0) up to and including the last character (shoppingItem.length - 1) This should be the 's', so now it becomes a singular item */
 
       else if(quantity > 1 && shoppingItem.substr(shoppingItem.length - 1) != 's' ) {
-        $("#myUL").append("<li> <input type='checkbox' name='done' class='itemDone'/>(x" + quantity + ") " + shoppingItem.toLowerCase() + " </li>" );
+        $("#myUL").append("<li> <input type='checkbox' name='done' class='itemDone'/>( " + quantity + unit +"s ) " + shoppingItem + "  <button class='delete'> x </button> </li>" );
       } /* Case of there being more than one quantity and the last character IS NOT an 's', leave the shoppingItem as is */
 
       else if(quantity == 1 && shoppingItem.substr(shoppingItem.length - 1) == 's'){
         shoppingItem = shoppingItem.substr(0, shoppingItem.length - 1);
-        $("#myUL").append("<li>  <input type='checkbox' name='done' class='itemDone'/>" + shoppingItem +  " </li>" );
+        $("#myUL").append("<li>  <input type='checkbox' name='done' class='itemDone'/>" + unit +" " +shoppingItem +  "<button class='delete'> x </button> </li>" );
       } /* Exactly 1 quantity, and the last character IS an 's', drop the 's'.*/
 
       else if(quantity == 1 && shoppingItem.substr(shoppingItem.length - 1) != 's'){
-        $("#myUL").append("<li>  <input type='checkbox' name='done' class='itemDone' /> " + shoppingItem +  " </li>" );
+        $("#myUL").append("<li>  <input type='checkbox' name='done' class='itemDone' /> " +  unit+" " + shoppingItem +  "<button class='delete'> x </button> </li>" );
       } /* Exactly 1 quantity, and the last character IS NOT an 's', leave as is*/
     } // The input field has a valid shoppingItem
-   $("#myItem").val();
+   $("#myItem").val('');
   }
  
   
@@ -43,13 +45,15 @@ $(document).ready(function() {
      $(this).parent().toggleClass('strike');
  } 
 
+  function deleteItem(){
+      $(this).parent().hide();
+  }
+  
     $(function(){
-    $("#add").on("click", addListItem) ;
+    $("#add").on("click", addListItem);
     $(document).on('click','.itemDone', doneBuyItem);
-
+     $(document).on('click','.delete', deleteItem);
   });
   
-
-
-  
 });
+
